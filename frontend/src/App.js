9e82aea3c15c02ffdc2deece0ec6de53db70b721
tokusakurai <Togo.39rai@gmail.com>
2024-03-29 13:39:27 +0900

    [add] react test app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function MyButton() {
+    return (
+        <button>
+            I'm a button
+        </button>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h1>Welcome to My App</h1>
+            <MyButton />
+        </div>
+    )
+}
