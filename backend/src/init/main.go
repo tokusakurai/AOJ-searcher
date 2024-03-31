b931665fdd256aae4e6d372f7a267977bfcb7f4f
tokusakurai <Togo.39rai@gmail.com>
2024-03-31 14:44:10 +0900

    [update] inital database setup

--- backend/src/init/main.go
+++ backend/src/init/main.go
@@ -2,42 +2,12 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
-
-type Submission struct {
-	JudgeId        int
-	UserId         string
-	ProblemId      string
-	Language       string
-	Version        string
-	SubmissionTime string
-	CpuTime        int
-	Memory         int
-}
-
-func getSubmissionFromMap(mapSubmission map[string]interface{}) Submission {
-	var submission Submission
-
-	submission.JudgeId = int(mapSubmission["judgeId"].(float64))
-	submission.UserId = mapSubmission["userId"].(string)
-	submission.ProblemId = mapSubmission["problemId"].(string)
-	submission.Language = mapSubmission["language"].(string)
-	submission.Version = mapSubmission["version"].(string)
-	submission.SubmissionTime = time.Unix(int64(mapSubmission["submissionDate"].(float64))/1000, 0).Format("2006-01-02 15:04:05")
-	submission.CpuTime = int(mapSubmission["cpuTime"].(float64))
-	submission.Memory = int(mapSubmission["memory"].(float64))
-
-	return submission
-}
 
 func dropTable(db *sql.DB) {
 	dropTableQuery := `DROP TABLE IF EXISTS SUBMISSIONS;`
@@ -67,40 +37,6 @@ func createTable(db *sql.DB) {
 	}
 }
 
-func insertSubmission(db *sql.DB, submission Submission) {
-	insertSubmissionQuery :=
-		`INSERT INTO SUBMISSIONS(
-			JUDGEID,
-			USERID,
-			PROBLEMID,
-			LANGUAGE,
-			VERSION,
-			SUBMISSIONTIME,
-			CPUTIME,
-			MEMORY
-		)
-		VALUES(
-			?, ?, ?, ?, ?, ?, ?, ?
-		)
-		ON DUPLICATE KEY UPDATE JUDGEID = JUDGEID`
-
-	_, err := db.Exec(
-		insertSubmissionQuery,
-		submission.JudgeId,
-		submission.UserId,
-		submission.ProblemId,
-		submission.Language,
-		submission.Version,
-		submission.SubmissionTime,
-		submission.CpuTime,
-		submission.Memory,
-	)
-
-	if err != nil {
-		log.Fatal("Error inserting submission data: ", err)
-	}
-}
-
 func main() {
 	host := os.Getenv("MYSQL_HOST")
 	port := os.Getenv("MYSQL_PORT")
@@ -123,54 +59,4 @@ func main() {
 
 	dropTable(db)
 	createTable(db)
-
-	url := "https://judgeapi.u-aizu.ac.jp/solutions/problems/1341"
-	response, err := http.Get(url)
-	if err != nil {
-		log.Fatal("Error http request: ", err)
-	}
-	defer response.Body.Close()
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal("Error reading response body: ", err)
-	}
-
-	var data []map[string]interface{}
-	err = json.Unmarshal(body, &data)
-	if err != nil {
-		log.Fatal("Error decoding json: ", err)
-	}
-
-	for _, submission := range data {
-		insertSubmission(db, getSubmissionFromMap(submission))
-	}
-
-	rows, err := db.Query(`SELECT * FROM SUBMISSIONS`)
-	if err != nil {
-		log.Fatal("Error selecting: ", err)
-	}
-	defer rows.Close()
-
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	for rows.Next() {
-		var submission Submission
-
-		rows.Scan(
-			&submission.JudgeId,
-			&submission.UserId,
-			&submission.ProblemId,
-			&submission.Language,
-			&submission.Version,
-			&submission.SubmissionTime,
-			&submission.CpuTime,
-			&submission.Memory,
-		)
-
-		fmt.Println(submission)
-	}
 }
