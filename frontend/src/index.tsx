2142fbcc6edc193c83b48d611f54b9cb5fba6062
tokusakurai <Togo.39rai@gmail.com>
2024-04-08 00:37:41 +0900

    [delete] React.StrictMode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 const root = document.getElementById('root');
 if (root) {
     ReactDOM.createRoot(root).render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <App />
     );
 }
 
